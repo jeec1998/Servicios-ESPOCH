5523b2b7b58e6a683d9cb029bb9984210280cdcc
lortiz <mariela.ortiz@espoch.edu.ec>
2023-11-08 10:25:23 -0500

    actualizacion de los servicios con el tratamiento de nombres especiales

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
     "user": "sa",
-    "password": "BDSqlAdmin111",
-     "server": "172.17.102.218",    
-//"password": "@SQLserver",
-  //"server": "172.17.103.26",
+  //  "password": "BDSqlAdmin111",
+   //  "server": "172.17.102.218",    
+"password": "@SQLserver",
+  "server": "172.17.103.26",
     "database": "SistemaAcademico"
 };

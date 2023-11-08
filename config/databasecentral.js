3016a268ecf5d17d176b861fe1de8c9f7a1724dd
lortiz <mariela.ortiz@espoch.edu.ec>
2023-11-08 09:40:15 -0500

    actualizacion de los servicios con el tratamiento de nombres especiales

--- config/databasecentral.js
+++ config/databasecentral.js
@@ -1,13 +1,13 @@
 module.exports = {
-    user: 'sistema',
+    /*user: 'sistema',
     host: 'localhost',
     database: 'bdCentralLocal',
     password: 'Sistemas',
     port: 5432,
-    
-    /*user: 'sistema',
+    */
+    user: 'sistema',
     host: '172.17.102.14',
     database: 'centralizacion_db',
     password: 'Sistemas',
-    port: 3311,*/
+    port: 3311,
 };

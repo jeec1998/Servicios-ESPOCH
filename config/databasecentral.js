bdd7f4b1c9f86c35deda2e592d372de647f12285
lortiz <mariela.ortiz@espoch.edu.ec>
2024-04-23 15:32:45 -0500

    Servicio para registrar una nueva persona o empresa

--- config/databasecentral.js
+++ config/databasecentral.js
@@ -1,14 +1,14 @@
 module.exports = {
-    user: 'sistema',
-    host: 'localhost',
-    database: 'bdCentralLocal',
-    password: 'Sistemas',
-    port: 5432,
-
-    /*
-     user: 'sistema',
-     host: '172.17.102.14',
-     database: 'centralizacion_db',
+    /* user: 'sistema',
+     host: 'localhost',
+     database: 'bdCentralLocal',
      password: 'Sistemas',
-     port: 3311,*/
+     port: 5432,
+ */
+
+    user: 'sistema',
+    host: '172.17.102.14',
+    database: 'centralizacion_db',
+    password: 'Sistemas',
+    port: 3311,
 };
